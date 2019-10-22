2f32dd2d19ad6ff288b971ec0401b81eecfcee20
amdedieu <amdedieu@student.42.fr>
2019-10-22 17:55:55 +0200

    retry

---
+++ ft_strdup.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amdedieu <amdedieu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/08 16:11:46 by amdedieu          #+#    #+#             */
+/*   Updated: 2019/10/15 14:16:06 by amdedieu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	char	*dst;
+	int		i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	if (!(dst = malloc(sizeof(char) * i + 1)))
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		dst[i] = s[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
